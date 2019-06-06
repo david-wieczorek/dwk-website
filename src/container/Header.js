cd025c94f720eb47e4ba9dc16e41bd5688b834bc
David Wieczorek <blackratio@gmail.com>
2019-06-06 18:46:29 +0200

    adding routes and container structure

---
+++ src/container/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../components/main_nav';
+
+// The Header creates links that can be used to navigate
+// between routes.
+const Header = () => (
+  <header>
+    <Nav />
+  </header>
+);
+
+export default Header;
