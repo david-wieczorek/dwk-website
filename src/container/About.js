4178ea2736b0d824c8960704ef579d73f5e47e89
David Wieczorek <blackratio@gmail.com>
2019-06-07 06:37:36 +0200

    bulma test

--- src/container/About.js
+++ src/container/About.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const About = () => (
   <div>
-    <h2>About</h2>
+    <section className="hero is-link is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Fullheight title</h1>
+          <h2 className="subtitle">Fullheight subtitle</h2>
+        </div>
+      </div>
+    </section>
+    <section className="hero is-medium is-light is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Primary bold title</h1>
+          <h2 className="subtitle">Primary bold subtitle</h2>
+        </div>
+      </div>
+    </section>
   </div>
 );
 
